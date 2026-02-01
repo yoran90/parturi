3588a6b5631e68149838845b0566f78912eecc62
bar2026 <your_email@example.com>
2026-02-01 22:28:12 +0200

    ok

--- server/app.js
+++ server/app.js
@@ -25,13 +25,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 app.use(cors({
   origin: "http://localhost:5173",
-  origin: ["http://localhost:5173", "http://localhost:3000"],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "Cache-Control", "Expires", "Pragma"],
   credentials: true
 }));
+
 
 
 const __filename = fileURLToPath(import.meta.url);
