b132fcb952fd4b8c9b28ebf4faa48612695a8baa
bar2026 <your_email@example.com>
2026-02-04 23:35:47 +0200

    change something

--- server/utlis/sendEmail.js
+++ server/utlis/sendEmail.js
@@ -4,14 +4,14 @@ import nodemailer from "nodemailer";
 const sendEmail = async (to, subject, text) => {
   try {
     const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD,
-  },
-});
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
 
 
     await transporter.sendMail({
