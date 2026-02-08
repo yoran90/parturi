0c8065953d299dd99ce6654caa04f618fd07651d
bar2026 <your_email@example.com>
2026-02-08 14:39:58 +0200

    loading edited

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -77,7 +77,7 @@ const App = () => {
   }, [dispatch]);
   
 
-  if (!loading) {
+  if (loading) {
     return (
       <div className="flex items-center justify-center h-screen w-full">
         <div className="loader">
