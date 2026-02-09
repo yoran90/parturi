4fb1c256ff3ae05dad812e039f3928a49e3b77c8
bar2026 <your_email@example.com>
2026-02-09 11:52:38 +0200

    some new change

--- server/app.js
+++ server/app.js
@@ -39,7 +39,7 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 //app.use('/sitemap.xml', express.static(path.join(__dirname, 'public', 'sitemap.xml')));
 // Serve robots.txt and sitemap.xml first
 app.get("/robots.txt", (req, res) => {
-  res.sendFile(path.resolve("./client/public/robots.txt"), err => {
+  res.sendFile(path.resolve("../client/public/robots.txt"), err => {
     if (err) {
       console.error("robots.txt error:", err);
       res.status(404).send("Not found");
@@ -49,7 +49,7 @@ app.get("/robots.txt", (req, res) => {
 
 // Serve sitemap.xml before React
 app.get("/sitemap.xml", (req, res) => {
-  res.sendFile(path.resolve("./client/public/sitemap.xml"), err => {
+  res.sendFile(path.resolve("../client/public/sitemap.xml"), err => {
     if (err) {
       console.error("sitemap.xml error:", err);
       res.status(404).send("Not found");
