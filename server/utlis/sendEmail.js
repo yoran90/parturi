0a24da76db40da2ca95d170dd5728ae18379ba0b
bar2026 <your_email@example.com>
2026-02-09 18:36:41 +0200

    some change

--- server/utlis/sendEmail.js
+++ server/utlis/sendEmail.js
@@ -1,24 +1,20 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",       
-  port: 587,  
-  secure: false,                       
-  /* auth: {
+  host: process.env.SMTP_HOST,       
+  port: parseInt(process.env.SMTP_PORT),  
+  secure: true,                       
+  auth: {
     user: process.env.NODEMAILER_EMAIL_USER,
     pass: process.env.NODEMAILER_EMAIL_PASSWORD,
-  }, */
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS
-  }
+  },
 });
 
 
 export const sendEmail = async ({ to, subject, text }) => {
   try {
     const info = await transporter.sendMail({
-      from: `"Razor parturi" <${process.env.SMTP_USER}>`,
+      from: `"Razor parturi" <${process.env.NODEMAILER_EMAIL_USER}>`,
       to,
       subject,
       text,
