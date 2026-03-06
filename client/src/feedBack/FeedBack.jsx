98b433d1469d80f088a2869b15ee081d211c86b3
bar2026 <your_email@example.com>
2026-03-06 18:48:56 +0200

    Working on it with feed back data

--- client/src/feedBack/FeedBack.jsx
+++ client/src/feedBack/FeedBack.jsx
@@ -47,7 +47,7 @@ const FeedBack = ({ onClose }) => {
 
     try {
         setLoading(true);
-        const resposnse = await axios.post("http://localhost:8001/api/feedBack/addFeedbackk", feedBackdata);
+        const resposnse = await axios.post("http://localhost:8001/api/feedBack/addFeedback", feedBackdata);
         toast.success(resposnse.data.message);
       } catch (error) {
         console.log(error);
