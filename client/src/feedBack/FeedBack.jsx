28f894cbbbc0e33731fd1c0e2176224877aa48a8
bar2026 <your_email@example.com>
2026-03-06 18:46:30 +0200

    Working on it with feed back data

--- client/src/feedBack/FeedBack.jsx
+++ client/src/feedBack/FeedBack.jsx
@@ -47,7 +47,7 @@ const FeedBack = ({ onClose }) => {
 
     try {
         setLoading(true);
-        const resposnse = await axios.post("https://parturi-backend.onrender.com/api/feedBack/addFeedback", feedBackdata);
+        const resposnse = await axios.post("http://localhost:8001/api/feedBack/addFeedbackk", feedBackdata);
         toast.success(resposnse.data.message);
       } catch (error) {
         console.log(error);
