9a0e3bb5b1ea87604b8079986c5c13330c82de87
bar2026 <your_email@example.com>
2026-03-10 22:48:40 +0200

    created cookie banner popup and functnolity

--- client/src/cookie-banner/CookieBanner.jsx
+++ client/src/cookie-banner/CookieBanner.jsx
@@ -57,7 +57,7 @@ const CookieBanner = () => {
 
 
   return (
-    <div className='fixed top-0 bottom-0 left-0 right-0 bg-black/30 z-100 flex flex-col items-center justify-center'>
+    <div className='fixed top-0 bottom-0 left-0 right-0 bg-black/50 z-100 flex flex-col items-center justify-center'>
       <div className='bg-white shadow md:w-full w-[98%] m-auto max-w-2xl p-4 rounded'>
         <div className='flex gap-4  justify-between'>
           <div className='flex items-center gap-0.5'>
