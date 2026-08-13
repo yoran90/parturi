dcbb25735b71ba84d1e7300a1719f086ad46480b
bar2026 <your_email@example.com>
2026-08-13 21:16:40 +0300

    Just change some style

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -350,8 +350,8 @@ const App = () => {
       {/* calandery */}
       {showCalandary && /* && !openFeedBack && visibleCalendar && */ (
           <div>
-            <button onClick={() => setOpenCalendar(!openCalendar)} className='fixed bottom-16 right-5  border-2 border-red-900 w-10 h-10 flex items-center justify-center bg-red-500 text-white text-sm rounded cursor-pointer gap-2.5'>
-              <img src="https://play-lh.googleusercontent.com/ttdkcjLXS5B8CLYYfCkrmMcjPk1jJjJZEIcd3S5eP9pLO48Yy3RnRnQgHy0n2WroqYA" alt="calandery" className="w-10 h-10" />
+            <button onClick={() => setOpenCalendar(!openCalendar)} className='fixed bottom-5 right-5  border-2 border-red-900 w-10 h-10 flex items-center justify-center bg-red-500 text-white text-sm rounded cursor-pointer gap-2.5'>
+              <img src="https://play-lh.googleusercontent.com/ttdkcjLXS5B8CLYYfCkrmMcjPk1jJjJZEIcd3S5eP9pLO48Yy3RnRnQgHy0n2WroqYA" alt="calandery" className="w-7 h-7" />
             </button>
 
             {openCalendar &&  (
